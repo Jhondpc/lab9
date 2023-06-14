ceeab49725954e388150bda42ebbf116a838fa6c
Jhondpc <a20196137@pucp.edu.pe>
2023-06-13 20:04:21 -0500

    aaa

---
+++ kkkkk/src/main/java/com/example/kkkkk/KkkkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkkkkApplication.class, args);
+    }
+
+}
